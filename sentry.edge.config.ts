c5ae4ffcbce79187d3ba55cebbbc3be369dba81e
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2024-10-25 19:49:14 +0100

    first commit

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,7 +6,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://a764d6aa1d66b9258b4cb094becdcb8e@o4507537249927168.ingest.de.sentry.io/4507537257922640",
+  dsn: "https://84d5bbf29c003212e6b55fb05f1df53e@o4507287963303936.ingest.de.sentry.io/4508184860164176",
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
